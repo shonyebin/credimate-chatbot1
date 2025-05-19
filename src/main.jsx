37942f40240a7b1e2dc9298c0864cde578e74315
shonyebin <syb0003084@naver.com>
2025-05-19 19:11:22 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import Login from './Login';
-import App from './App';
-import './index.css';
-
-const Root = () => {
-  const [student, setStudent] = useState(null);
-  return student ? <App student={student} /> : <Login onLogin={setStudent} />;
-};
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './Login'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
-  </React.StrictMode>
-);
+    <Login />
+  </React.StrictMode>,
+)
