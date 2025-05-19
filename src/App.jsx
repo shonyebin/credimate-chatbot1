d38866d6e068c0c66a801567da5c15ebefbd2e71
shonyebin <syb0003084@naver.com>
2025-05-19 19:52:52 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-import React, { useState } from 'react'
-import Chatbot from './Chatbot'
-import Login from './Login'
-
-function App() {
-  const [studentId, setStudentId] = useState('')
-  const [loggedIn, setLoggedIn] = useState(false)
-
-  const handleLogin = (id) => {
-    setStudentId(id)
-    setLoggedIn(true)
-  }
-
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>CrediMate 학생지원 챗봇</h1>
-      {!loggedIn ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <>
-          <p>🤖: 토론</p>
-          <Chatbot studentId={studentId} />
-        </>
-      )}
-    </div>
-  )
-}
-
-export default App
+import React from 'react';
+const App = () => <h1>CrediMate 학생지원 챗봇 준비 중입니다.</h1>;
+export default App;
